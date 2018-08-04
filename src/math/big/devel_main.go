8d24fc0b20c4f88298b9f73e66555969c84b53dc
Armando Faz <armfazh@cloudflare.com>
2018-08-03 17:09:16 -0700

    Working on general version for integer multiplication.

--- src/math/big/devel_main.go
+++ src/math/big/devel_main.go
@@ -86,15 +86,29 @@ func test_montgo() {
 }
 
 func test_intmult() {
-	x := natFromString2("0x2acb12106b44f6632bef94318715f512852b73a92340543c1b72899ebc7ac24816c39c7b1d67528d8a3927ef14a22434bbac41e6731f52ee8f0e425b9df0cc7122f85255696e7019d5f88d062b1c356fb09757f6b2c8ea38f4bff44f757afe8bac0d2e7e0b946169b349eb4178309597f7b537ef4015bb61ac229d29c94a773470984321709832174210342187340832142431513")
-	y := natFromString2("0x0732104832148325826538246381274832740126136532643265326de8e597bf03ee5209a2395dbbad963ddc86f9dcfc32f8c91db039b388b5a78462a8ca7913eba135c146085fa317da0dd02031b11518eb781f9f945057e9b341c902e26a0c13f563dcce8b5805fd69bbd8160640b738c8db8683653b4336b1176b2801892196a6e1b3fe88e51d6476f86e47d99d4d67ee861d4321423ef612c3877cf07")
+
+	n := 8
+	x := nat(nil).make(n)
+	y := nat(nil).make(n)
+	m := nat(nil).make(n)
+
+	for i := range x {
+		x[i] = 0xffffffffffffffff
+	}
+	for i := range y {
+		y[i] = 0xffffffffffffffff
+	}
+	for i := range m {
+		m[i] = 0xffffffffffffffff
+	}
+
 	x.PrintHex()
 	y.PrintHex()
 
 	z := nat(nil).make(len(x) + len(y))
 	z.clear()
 	fmt.Printf("lx: %d ly: %d\n", len(x), len(y))
-
+	intmadd512Nx512N(z, x[:5], y)
 	z.PrintHex()
 }
 
@@ -143,7 +157,7 @@ func test_nat() {
 func (i Int) MasPruebas() {
 
 	//	test_nat()
-	//test_intmult()
-	test_montgo()
+	test_intmult()
+	//test_montgo()
 
 }
